bfeb7da03854f1769594b34b95f497a0ba7d3eaf
Jordan Wagner <jordan.c.wagner@gmail.com>
2020-05-18 16:44:53 -0700

    Changed from arrays to linked lists

--- apple_questions_1.c
+++ apple_questions_1.c
@@ -21,122 +21,232 @@ Bonus: Output how many rounds should be completed before the deck is adequately 
 #include <stdio.h>
 #include <stdlib.h>
 
-// Checks if the deck is sorted by comparing each value to it's index in the deck
-bool is_sorted(uint32_t * deck, uint32_t deck_size) {
-  for (uint32_t i = 0; i < deck_size; i++) {
-    if (deck[i] != i) {
+// Structure of a card in the linked list deck
+typedef struct card {
+  // Value of the card, used for determining if the deck is back to original order
+  uint32_t card_num;
+  struct card *next;
+} card_t;
+
+// Checks if the deck is sorted (lowest card_num first)
+bool is_sorted(card_t *deck) {
+  if (deck == NULL) {
+    printf("%s: deck is NULL!\r\n", __FUNCTION__);
+    return false;
+  }
+  
+  while (deck->next != NULL) {
+    card_t *next = deck->next;
+    if (deck->card_num >= next->card_num) {
       return false;
     }
+    deck = deck->next;
   }
   
   return true;
 }
 
 // Prints out the deck in the current order
-void print_deck(uint32_t * deck, uint32_t deck_size) {
-  for (uint32_t i = 0; i < deck_size; i++) {
-    printf("%4u", deck[i]);
+void print_deck(card_t *deck) {
+  if (deck == NULL) {
+    printf("%s: deck is NULL!\r\n", __FUNCTION__);
+    return;
+  }
+  
+  while (deck != NULL) {
+    printf("%4u", deck->card_num);
+    deck = deck->next;
   }
   printf("\r\n");
 }
 
 // Function for dealing cards into three piles
-void three_piles(uint32_t * deck, uint32_t * pile1, uint32_t * pile2, uint32_t * pile3, uint32_t deck_size) {
-  // Pile indexes
-  uint32_t p1 = 0, p2 = 0, p3 = 0;
-  for(uint32_t i = 0; i < deck_size; i++) {
-    if ((i % 3) == 0) {
-      pile1[p1++] = deck[i];
-    } else if ((i % 3) == 1) {
-      pile2[p2++] = deck[i];
-    } else if ((i % 3) == 2) {
-      pile3[p3++] = deck[i];
+// Uses three stacks to push cards into, then just links all the stacks together and returns
+card_t * three_piles(card_t *deck) {
+  if (deck == NULL) {
+    printf("%s: deck is NULL!\r\n", __FUNCTION__);
+    return NULL;
+  }
+  // Pile stack pointers
+  card_t *p1_stack = NULL, *p2_stack = NULL, *p3_stack = NULL;
+  // Keep track of the bottom of piles 1 and 2 for rebuilding the deck
+  card_t *p1_end = NULL, *p2_end = NULL;
+  // Counter to keep track of which pile to add a card to
+  uint32_t pile_count = 0;
+  
+  while (deck != NULL) {
+    card_t * temp = deck->next;
+    if (pile_count % 3 == 0) {
+      if (p1_stack == NULL) {
+        p1_stack = deck;
+        p1_end = deck;
+        p1_stack->next = NULL;
+      } else {
+        deck->next = p1_stack;
+        p1_stack = deck;
+      }
+    } else if (pile_count % 3 == 1) {
+      if (p2_stack == NULL) {
+        p2_stack = deck;
+        p2_end = deck;
+        p2_stack->next = NULL;
+      } else {
+        deck->next = p2_stack;
+        p2_stack = deck;
+      }
+    } else if (pile_count % 3 == 2) {
+      if (p3_stack == NULL) {
+        p3_stack = deck;
+        p3_stack->next = NULL;
+      } else {
+        deck->next = p3_stack;
+        p3_stack = deck;
+      }
     }
+    pile_count++;
+    deck = temp;
   }
   
-  // Copy each pile back to the deck in "reverse" order
-  // Each pile is essentially a stack, so this preserves the LIFO ordering
-  uint32_t deck_ptr = 0;
-  for(uint32_t i = p1; i > 0; i--) {
-    deck[deck_ptr++] = pile1[i - 1];
-  }
-  for(uint32_t i = p2; i > 0; i--) {
-    deck[deck_ptr++] = pile2[i - 1];
-  }
-  for(uint32_t i = p3; i > 0; i--) {
-    deck[deck_ptr++] = pile3[i - 1];
-  }
+  // Rebuild the deck by simply pointing the bottom of p1 to the top of p2 and the bottom of p2 to the top of p3
+  p1_end->next = p2_stack;
+  p2_end->next = p3_stack;
+  return p1_stack;
 }
 
 // Function for dealing cards into four piles
-void four_piles(uint32_t * deck, uint32_t * pile1, uint32_t * pile2, uint32_t * pile3, uint32_t * pile4, uint32_t deck_size) {
-  // Pile indexes
-  uint32_t p1 = 0, p2 = 0, p3 = 0, p4 = 0;
-  for(uint32_t i = 0; i < deck_size; i++) {
-    if ((i % 4) == 0) {
-      pile1[p1++] = deck[i];
-    } else if ((i % 4) == 1) {
-      pile2[p2++] = deck[i];
-    } else if ((i % 4) == 2) {
-      pile3[p3++] = deck[i];
-    } else if ((i % 4) == 3) {
-      pile4[p4++] = deck[i];
+// Uses four stacks to push cards into, then just links all the stacks together and returns
+card_t * four_piles(card_t *deck) {
+  if (deck == NULL) {
+    printf("%s: deck is NULL!\r\n", __FUNCTION__);
+    return NULL;
+  }
+  // Pile stack pointers
+  card_t *p1_stack = NULL, *p2_stack = NULL, *p3_stack = NULL, *p4_stack = NULL;
+  // Keep track of the bottom of piles 1, 2, and 3 for rebuilding the deck
+  card_t *p1_end = NULL, *p2_end = NULL, *p3_end = NULL;
+  // Counter to keep track of which pile to add a card to
+  uint32_t pile_count = 0;
+  
+  while (deck != NULL) {
+    card_t * temp = deck->next;
+    if (pile_count % 4 == 0) {
+      if (p1_stack == NULL) {
+        p1_stack = deck;
+        p1_end = deck;
+        p1_stack->next = NULL;
+      } else {
+        deck->next = p1_stack;
+        p1_stack = deck;
+      }
+    } else if (pile_count % 4 == 1) {
+      if (p2_stack == NULL) {
+        p2_stack = deck;
+        p2_end = deck;
+        p2_stack->next = NULL;
+      } else {
+        deck->next = p2_stack;
+        p2_stack = deck;
+      }
+    } else if (pile_count % 4 == 2) {
+      if (p3_stack == NULL) {
+        p3_stack = deck;
+        p3_end = deck;
+        p3_stack->next = NULL;
+      } else {
+        deck->next = p3_stack;
+        p3_stack = deck;
+      }
+    } else if (pile_count % 4 == 3) {
+      if (p4_stack == NULL) {
+        p4_stack = deck;
+        p4_stack->next = NULL;
+      } else {
+        deck->next = p4_stack;
+        p4_stack = deck;
+      }
     }
+    pile_count++;
+    deck = temp;
   }
   
-  // Copy each pile back to the deck in "reverse" order
-  // Each pile is essentially a stack, so this preserves the LIFO ordering
-  uint32_t deck_ptr = 0;
-  for(uint32_t i = p1; i > 0; i--) {
-    deck[deck_ptr++] = pile1[i - 1];
-  }
-  for(uint32_t i = p2; i > 0; i--) {
-    deck[deck_ptr++] = pile2[i - 1];
-  }
-  for(uint32_t i = p3; i > 0; i--) {
-    deck[deck_ptr++] = pile3[i - 1];
-  }
-  for(uint32_t i = p4; i > 0; i--) {
-    deck[deck_ptr++] = pile4[i - 1];
-  }
+  // Rebuild the deck by simply modifying the bottom of the stack pointers
+  p1_end->next = p2_stack;
+  p2_end->next = p3_stack;
+  p3_end->next = p4_stack;
+  return p1_stack;
 }
 
 // Function for dealing cards into five piles
-void five_piles(uint32_t * deck, uint32_t * pile1, uint32_t * pile2, uint32_t * pile3, uint32_t * pile4, uint32_t * pile5, uint32_t deck_size) {
-  // Pile indexes
-  uint32_t p1 = 0, p2 = 0, p3 = 0, p4 = 0, p5 = 0;
-  for(uint32_t i = 0; i < deck_size; i++) {
-    if ((i % 5) == 0) {
-      pile1[p1++] = deck[i];
-    } else if ((i % 5) == 1) {
-      pile2[p2++] = deck[i];
-    } else if ((i % 5) == 2) {
-      pile3[p3++] = deck[i];
-    } else if ((i % 5) == 3) {
-      pile4[p4++] = deck[i];
-    } else if ((i % 5) == 4) {
-      pile5[p5++] = deck[i];
+// Uses five stacks to push cards into, then just links all the stacks together and returns
+card_t * five_piles(card_t *deck) {
+  if (deck == NULL) {
+    printf("%s: deck is NULL!\r\n", __FUNCTION__);
+    return NULL;
+  }
+  // Pile stack pointers
+  card_t *p1_stack = NULL, *p2_stack = NULL, *p3_stack = NULL, *p4_stack = NULL, *p5_stack = NULL;
+  // Keep track of the bottom of piles 1, 2, 3, and 4 for rebuilding the deck
+  card_t *p1_end = NULL, *p2_end = NULL, *p3_end = NULL, *p4_end = NULL;
+  // Counter to keep track of which pile to add a card to
+  uint32_t pile_count = 0;
+  
+  while (deck != NULL) {
+    card_t * temp = deck->next;
+    if (pile_count % 5 == 0) {
+      if (p1_stack == NULL) {
+        p1_stack = deck;
+        p1_end = deck;
+        p1_stack->next = NULL;
+      } else {
+        deck->next = p1_stack;
+        p1_stack = deck;
+      }
+    } else if (pile_count % 5 == 1) {
+      if (p2_stack == NULL) {
+        p2_stack = deck;
+        p2_end = deck;
+        p2_stack->next = NULL;
+      } else {
+        deck->next = p2_stack;
+        p2_stack = deck;
+      }
+    } else if (pile_count % 5 == 2) {
+      if (p3_stack == NULL) {
+        p3_stack = deck;
+        p3_end = deck;
+        p3_stack->next = NULL;
+      } else {
+        deck->next = p3_stack;
+        p3_stack = deck;
+      }
+    } else if (pile_count % 5 == 3) {
+      if (p4_stack == NULL) {
+        p4_stack = deck;
+        p4_end = deck;
+        p4_stack->next = NULL;
+      } else {
+        deck->next = p4_stack;
+        p4_stack = deck;
+      }
+    } else if (pile_count % 5 == 4) {
+      if (p5_stack == NULL) {
+        p5_stack = deck;
+        p5_stack->next = NULL;
+      } else {
+        deck->next = p5_stack;
+        p5_stack = deck;
+      }
     }
+    pile_count++;
+    deck = temp;
   }
   
-  // Copy each pile back to the deck in "reverse" order
-  // Each pile is essentially a stack, so this preserves the LIFO ordering
-  uint32_t deck_ptr = 0;
-  for(uint32_t i = p1; i > 0; i--) {
-    deck[deck_ptr++] = pile1[i - 1];
-  }
-  for(uint32_t i = p2; i > 0; i--) {
-    deck[deck_ptr++] = pile2[i - 1];
-  }
-  for(uint32_t i = p3; i > 0; i--) {
-    deck[deck_ptr++] = pile3[i - 1];
-  }
-  for(uint32_t i = p4; i > 0; i--) {
-    deck[deck_ptr++] = pile4[i - 1];
-  }
-  for(uint32_t i = p5; i > 0; i--) {
-    deck[deck_ptr++] = pile5[i - 1];
-  }
+  // Rebuild the deck by simply modifying the bottom of the stack pointers
+  p1_end->next = p2_stack;
+  p2_end->next = p3_stack;
+  p3_end->next = p4_stack;
+  p4_end->next = p5_stack;
+  return p1_stack;
 }
 
 void main(int argc, char **argv) {
@@ -146,61 +256,56 @@ void main(int argc, char **argv) {
   }
   uint32_t deck_size = atoi(argv[1]);
   
-  uint32_t * deck = (uint32_t *)malloc(deck_size * sizeof(uint32_t));
-  // Each of the first three piles will hold at most a third of the deck (rounded up)
-  uint32_t * pile1 = (uint32_t *)malloc(((deck_size / 3) + 1) * sizeof(uint32_t));
-  uint32_t * pile2 = (uint32_t *)malloc(((deck_size / 3) + 1) * sizeof(uint32_t));
-  uint32_t * pile3 = (uint32_t *)malloc(((deck_size / 3) + 1) * sizeof(uint32_t));
-  // The fourth pile will need to hold 1/4th of the deck at most (rounded up)
-  uint32_t * pile4 = (uint32_t *)malloc(((deck_size / 4) + 1) * sizeof(uint32_t));
-  // The fifth pile will only need to hold 1/5th of the deck (rounded up)
-  uint32_t * pile5 = (uint32_t *)malloc(((deck_size / 5) + 1) * sizeof(uint32_t));
-  if (deck == NULL || pile1 == NULL || pile2 == NULL || pile3 == NULL || pile4 == NULL || pile5 == NULL) {
-    free(deck);
-    free(pile1);
-    free(pile2);
-    free(pile3);
-    free(pile4);
-    free(pile5);
-    printf("Error allocating memory!\r\n");
-    return;
-  }
-  
+  card_t *deck = NULL, *prev = NULL;
   for (uint32_t i = 0; i < deck_size; i++) {
-    // Set each card value to be the same as it's index
-    deck[i] = i;
+    card_t *node = malloc(sizeof(card_t));
+    if (node == NULL) {
+      printf("Error allocating card #%u\r\n", i);
+      return;
+    }
+    node->card_num = i;
+    node->next = NULL;
+    if (deck == NULL) {
+      // Point the deck pointer to the first card
+      deck = node;
+    } else if (prev == NULL) {
+      // Initialize prev to the first card in the deck
+      prev = node;
+      deck->next = node;
+    } else {
+      prev->next = node;
+      prev = node;
+    }
   }
   
   uint32_t shuffle_times = 0;
-  //print_deck(deck, deck_size);
-  
-  while(true) {
-    three_piles(deck, pile1, pile2, pile3, deck_size);
+  //print_deck(deck);
+  while (true) {
+    deck = three_piles(deck);
     shuffle_times++;
-    //print_deck(deck, deck_size);
-    if (is_sorted(deck, deck_size)) {
+    //print_deck(deck);
+    if (is_sorted(deck)) {
       break;
     }
-    four_piles(deck, pile1, pile2, pile3, pile4, deck_size);
+    deck = four_piles(deck);
     shuffle_times++;
-    //print_deck(deck, deck_size);
-    if (is_sorted(deck, deck_size)) {
+    //print_deck(deck);
+    if (is_sorted(deck)) {
       break;
     }
-    five_piles(deck, pile1, pile2, pile3, pile4, pile5, deck_size);
+    deck = five_piles(deck);
     shuffle_times++;
-    //print_deck(deck, deck_size);
-    if (is_sorted(deck, deck_size)) {
+    //print_deck(deck);
+    if (is_sorted(deck)) {
       break;
     }
   }
   
   printf("Number of shuffles: %u\r\n", shuffle_times);
   
-  free(deck);
-  free(pile1);
-  free(pile2);
-  free(pile3);
-  free(pile4);
-  free(pile5);
+  while (deck != NULL) {
+    card_t *next = deck->next;
+    free(deck);
+    deck = next;
+  }
 }
